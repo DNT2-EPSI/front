1580141c1abefa916a3687dcbd463e1288005b53
ALEXIS GUETAT <alexis.guetat34@hotmail.fr>
2020-03-24 12:23:19 +0100

    add recup chantiers et centres

--- src/app/component/centres/centres.component.ts
+++ src/app/component/centres/centres.component.ts
@@ -31,4 +31,45 @@ export class CentresComponent implements OnInit {
         }
       );
   }
+
+  downloadButtonPush() {
+    console.log("Lancement de l'import");
+    var csvData = this.ConvertToCSV();
+    var a = document.createElement("a");
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+    var blob = new Blob([csvData], { type: 'text/csv' });
+    var url = window.URL.createObjectURL(blob);
+    console.log("Import en cours");
+    a.href = url;
+    a.download = 'Centres.csv';
+    a.click();
+    console.log("Import finis");
+
+}
+
+ConvertToCSV(): string {
+    var array = JSON.parse(JSON.stringify(this.centres));
+    var str = '';
+    var row = "";
+
+    for (var index in this.centres[0]) {
+        //Now convert each value to string and comma-separated
+        row += index + ',';
+    }
+    row = row.slice(0, -1);
+    //append Label row with line break
+    str += row + '\r\n';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+        str += line + '\r\n';
+    }
+    return str;
+}
 }
--- src/app/component/chantiers/chantiers-perturbants.component.ts
+++ src/app/component/chantiers/chantiers-perturbants.component.ts
@@ -32,4 +32,45 @@ export class ChantiersPerturbantsComponent implements OnInit {
       );
   }
 
+  downloadButtonPush() {
+    console.log("Lancement de l'import");
+    var csvData = this.ConvertToCSV();
+    var a = document.createElement("a");
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+    var blob = new Blob([csvData], { type: 'text/csv' });
+    var url = window.URL.createObjectURL(blob);
+    console.log("Import en cours");
+    a.href = url;
+    a.download = 'Chantiers.csv';
+    a.click();
+    console.log("Import finis");
+
+}
+
+ConvertToCSV(): string {
+    var array = JSON.parse(JSON.stringify(this.chantiers));
+    var str = '';
+    var row = "";
+
+    for (var index in this.chantiers[0]) {
+        //Now convert each value to string and comma-separated
+        row += index + ',';
+    }
+    row = row.slice(0, -1);
+    //append Label row with line break
+    str += row + '\r\n';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+        str += line + '\r\n';
+    }
+    return str;
+}
+
 }
