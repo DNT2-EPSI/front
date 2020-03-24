b259c5f5b4ee167701f07392e1ecd94fee5782a6
ALEXIS GUETAT <alexis.guetat34@hotmail.fr>
2020-03-24 11:35:52 +0100

    add recup eclairages

--- src/app/component/eclairages/eclairage-public.component.ts
+++ src/app/component/eclairages/eclairage-public.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
-
 @Component({
   selector: 'app-eclairage-public',
   templateUrl: './eclairage-public.component.html',
@@ -34,5 +33,23 @@ export class EclairagePublicComponent implements OnInit {
       );
   }
 
+  downloadFile() {
+    var data = this.eclairages ;
+    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+    csv.unshift(header.join(','));
+    let csvArray = csv.join('\r\n');
+
+    var a = document.createElement('a');
+    var blob = new Blob([csvArray], {type: 'text/csv' }),
+    url = window.URL.createObjectURL(blob);
+
+    a.href = url;
+    a.download = "myFile.csv";
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+}
 
 }
